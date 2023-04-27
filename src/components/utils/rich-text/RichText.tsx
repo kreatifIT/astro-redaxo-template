21335f209ea58413a92f6e1bd6f4037fe9a3ba1c
Daniel Planötscher <d.planoetscher@kreatif.it>
2023-04-27 15:24:46 +0200

    fixed some default stylings

---
+++ src/components/utils/rich-text/RichText.tsx
@@ -0,0 +1,15 @@
+import './RichText.scss';
+interface Props {
+    text: string;
+    class?: string;
+}
+
+export default function RichText({ text, class: className }: Props) {
+    if (!text) return null;
+    return (
+        <div
+            className={'rich-text ' + (className ?? '')}
+            dangerouslySetInnerHTML={{ __html: text }}
+        />
+    );
+}
