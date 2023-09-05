c588a0fb35b2f1a3c772dd02c4fc6cfa3da9c7d7
Vicky Pallaoro <v.pallaoro@kreatif.it>
2023-09-05 15:06:55 +0200

    viewtransitions and recaptcha

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,9 @@ export default defineConfig({
             },
         }),
     ],
+    experimental: {
+        viewTransitions: true,
+    },
     vite: {
         ssr: {
             noExternal: [
