776a7914e9bc11ea96896c3c46fa25ee58f3cc48
Vicky Pallaoro <v.pallaoro@kreatif.it>
2023-09-06 10:40:49 +0200

    integrate framer motion, use it for popup

--- src/components/utils/modalpopup/ModalPopupWrapper.tsx
+++ src/components/utils/modalpopup/ModalPopupWrapper.tsx
@@ -1,33 +1,18 @@
-import { useEffect, useState } from 'preact/hooks';
-import { createPortal } from 'preact/compat';
-import './ModalPopup.scss';
+import { createPortal, useEffect, useState } from 'preact/compat';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface Props {
     children: any;
     visible: boolean;
-    closeAction?: () => void;
 }
 
-export default function ModalPopup({ children, visible, closeAction }: Props) {
+export default function PopupWrapper({ children, visible }: Props) {
     const [shown, setShown] = useState<boolean>(false);
-    const closingTag = 'A';
     useEffect(() => {
         if (visible) {
             document.body.classList.add('overflow-hidden');
         } else {
             document.body.classList.remove('overflow-hidden');
-        }
-    }, [visible]);
-
-    useEffect(() => {
-        if (visible) {
-            document.addEventListener('click', (element) => {
-                const clickTarget = element.target as HTMLElement;
-                const isLink = clickTarget.tagName == closingTag || clickTarget?.parentNode?.tagName == closingTag;
-                if (element?.target?.classList?.contains('popup-inner') || isLink) {
-                    closeAction?.();
-                }
-            });
         }
     }, [visible]);
 
@@ -43,19 +28,26 @@ export default function ModalPopup({ children, visible, closeAction }: Props) {
 
     if (typeof document === undefined) return null;
     if (!shown) return null;
-    const target = document.querySelector('#popup-target')!
-    if (!target) return null;
     return createPortal(
-        <div
-            class={[
-                'modalpopup-wrapper',
-                visible ? '' : 'modalpopup-out',
-                "fixed inset-0 after:bg-gray-900 after:opacity-70 after:absolute after:inset-0 after:-z-10"].join(' ')}>
-            <div className="inner absolute inset-0 modalpopup-inner">
-                {children}
-            </div>
-        </div>,
-        target
+        <AnimatePresence>
+            {visible && (
+                <motion.div
+                    initial={{ opacity: 0, translateY: "100%" }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    exit={{ opacity: 0, translateY: "100%" }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                    class={[
+                        'popup-wrapper',
+                        "fixed inset-0 after:bg-white after:opacity-95 after:absolute after:inset-0 after:-z-10"].join(' ')}>
+                    <div className="inner absolute inset-0 top-0 b-0 md:flex md:flex-col md:justify-center">
+                        {children}
+                    </div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+
+        ,
+        document.querySelector('#popup-target')!
     );
 
 }
