cccf4c4f1019f8b03aec9898102e791e37c41083
Vicky Pallaoro <v.pallaoro@kreatif.it>
2023-09-07 15:43:47 +0200

    skeleton styles

--- src/components/skeleton/DummySkeleton.tsx
+++ src/components/skeleton/DummySkeleton.tsx
@@ -1,10 +1,12 @@
+import './Skeleton.scss';
+
 interface Props {
     amount: number;
 }
 
 export default function Dummyskeleton({ amount = 1 }) {
     return (
-        <div class="lg:grid lg:grid-cols-2 gap-6 lg:gap-10 items-center xl:gap-[60px] mb-10 opacity-50">
+        <div data-skeleton class="lg:grid lg:grid-cols-2 gap-6 lg:gap-10 items-center xl:gap-[60px] mb-10 opacity-50">
             <div class="aspect-[214/150] bg-gray-200"></div>
             <div class="flex flex-col p-6 gap-6 md:px-0 xl:p-0">
                 <div class="h-10 bg-gray-200"></div>
