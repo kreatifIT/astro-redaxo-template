f521ee51ecb2d07177501ab23564bc6c5ba37337
Vicky Pallaoro <v.pallaoro@kreatif.it>
2024-02-20 13:41:14 +0100

    add map mparker and hidden font configurations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,10 +21,24 @@ const config = {
                         linkedin: '#0077b5',
                     },
                 },
-                fontFamily: {
-                    sans: ['', ...defaultTheme.fontFamily.sans],
-                }
             },
+            fontFamily: {
+                sans: ['', ...defaultTheme.fontFamily.sans],
+            },
+        //     fontSize: {
+        //         'xs': ['0.78125rem', '140%'], // xs // caption / 10.13 / 130
+        //         'sm': ['0.84375rem', '140%'], // sm // 13.5  / 140%
+        //         'lg': ['1.125rem', '140%'], // text-lg  // body text actually
+        //         '2xl': ['1.5rem', '133%'], // h5 > 24, 133%
+        //         '3xl': ['2rem', '112.6'], // h4 > 32, 112.6
+        //         '4xl': ['2.675rem', '112.6%'], // h3 > 42.63, 120% 
+        //         '5xl': ['3.5rem', '112.6%'], // h2  > 56.8, 112.6%
+        //         '8xl': ['6rem', '100px'], // h1 > 96 / 100px
+        //    },
+        //    screens: {
+        //        'md': '704px',
+        //         '4xl': '1920px',
+        //    },
         },
     },
 };
