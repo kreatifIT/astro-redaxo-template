a2b4e7430814fa21f2f018f641eea893fc5c6349
Vicky Pallaoro <v.pallaoro@kreatif.it>
2024-05-07 11:32:51 +0200

    container add nogap option

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -7,12 +7,14 @@ interface Props {
 export default function Container({
     class: className = '',
     noPadding = false,
+    noGap = false,
     children,
 }: Props) {
     return (
         <div
             class={[
                 'max-w-[1680px] mx-auto relative z-1 w-full grid',
+                !noGap ? 'gap-4' : '',
                 !noPadding ? 'px-4' : '',
                 className,
             ].join(' ')}
